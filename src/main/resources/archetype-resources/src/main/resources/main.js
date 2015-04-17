fc2d91ac5b9ad6aea685d6bda7fc960d5221e8ad
Deven Phillips <deven.phillips@gmail.com>
2015-04-17 11:57:01 -0400

    Reworked stub and updated version

--- src/main/resources/archetype-resources/src/main/resources/main.js
+++ src/main/resources/archetype-resources/src/main/resources/main.js
@@ -1,2 +1,7 @@
-var Vertx = require("vertx-js/vertx");
-var vertx = Vertx.vertx();
+var server = vertx.createHttpServer();
+
+server.requestHandler(function(req) {
+  req.response().putHeader("Content-Type", "text/html").end("<html><body><h1>Hello Vert.x</h1></body></html>");
+});
+
+server.listen(8080);
